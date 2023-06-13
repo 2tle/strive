b39cec4903f44c1a9c187200fde911445bd7d515
Hyunjun Yang <iam@2tle.io>
2023-06-13 16:15:00 +0900

    Feat: add changing char feature
    로비씬에서 캐릭터 변경 기능 추가

---
+++ Assets/30610/Scripts/LobbyCharacterManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyCharacterManager : MonoBehaviour
+{
+    public Vector3 lbSpawnPos = new Vector3(17f, -21f, 887f);
+    public Button leftBtn;
+    public Button rightBtn;
+    public Button infoBtn;
+    public Text infoText;
+    public GameObject[] playerPrefabs;
+    public GameObject player;
+    public string[] playersDesc;
+    private int currentPlayers = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftBtn = GameObject.Find("LeftButton").GetComponent<Button>();
+        rightBtn = GameObject.Find("RightButton").GetComponent<Button>();
+        infoBtn = GameObject.Find("CharInfoButton").GetComponent<Button>();
+        //infoText = GameObject.Find("CharInfoText").GetComponent<Text>();
+        //player = GameObject.Find("PlayerCharacter").GetComponent<GameObject>();
+
+        infoText.text = "";
+        infoText.gameObject.SetActive(false);
+
+        leftBtn.onClick.AddListener(onLeftBtnClicked);
+        rightBtn.onClick.AddListener(onRightBtnClicked);
+        infoBtn.onClick.AddListener(onInfoBtnClicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void onLeftBtnClicked()
+    {
+        if (currentPlayers == 0) currentPlayers = playerPrefabs.Length;
+        currentPlayers--;
+
+        spawnChar(currentPlayers);
+        
+    }
+    private void onRightBtnClicked()
+    {
+        if (currentPlayers == playerPrefabs.Length - 1) currentPlayers = -1;
+        currentPlayers++;
+
+        spawnChar(currentPlayers);
+    }
+    private void onInfoBtnClicked()
+    {
+        //설명창 띄우기.
+        if (infoText.gameObject.activeSelf == true) {
+            infoText.gameObject.SetActive(false);
+            return;
+        }
+
+        infoText.text = playersDesc[currentPlayers];
+        infoText.gameObject.SetActive(true);
+
+    }
+    private void spawnChar(int pos) {
+        Destroy(player);
+        player = Instantiate(playerPrefabs[pos]);
+        player.transform.position = lbSpawnPos;
+        infoText.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/30610/Scripts/LobbyQueueManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyQueueManager : MonoBehaviour
+{
+    public InputField usernameInputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Photon Init
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
