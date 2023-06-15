1495d9f3873dafdc7cc851295971db9c5a8ae2a4
Hyunjun Yang <iam@2tle.io>
2023-06-15 15:05:31 +0900

    Feat: cinemachine vcam mouse follow
    카메라 마우스 따라가는거 이재균이 시킴.

--- Assets/30610/Scripts/InitializeManager.cs
+++ Assets/30610/Scripts/InitializeManager.cs
@@ -14,6 +14,9 @@ public class InitializeManager : MonoBehaviour
 
     public GameObject player;
 
+    public float mouseSensitivity = 2f;
+    private Vector2 rotation = Vector2.zero;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,16 @@ public class InitializeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 마우스 입력 감지
+        rotation.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotation.y -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, -90f, 90f); // 수직 회전 제한
+
+        // 캐릭터의 시야 회전 설정
+        player.transform.rotation = Quaternion.Euler(0f, rotation.x, 0f);
+
+        // Cinemachine 가상 카메라의 트랜스폼 설정
+        vCam.transform.rotation = Quaternion.Euler(rotation.y, rotation.x, 0f);
+    
     }
 }
